d390d74acacce4db5fc8122a222a190f928c2faa
Marcos Cisneros <marcoscisneros674@gmail.com>
2023-09-21 21:53:51 -0300

    gran actualizacion, el proyecto pasa a su segunda version, responsive y codigo legible

--- js/carrito.js
+++ js/carrito.js
@@ -1,162 +1,93 @@
-// Llamados del DOM
-let productosEnCarro = JSON.parse(localStorage.getItem("productos-en-carrito"));
-const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
-const contenedorCarritoProductos = document.querySelector(
-  "#contenedor-carrito-productos"
-);
-const carritoAcciones = document.querySelector("#carrito-acciones");
-const carritoComprado = document.querySelector("#carrito-comprado");
-const botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
 
-const btnVaciar = document.getElementById("btnVaciar");
-btnVaciar.addEventListener("click", vaciarCarrito);
+principal();
+//mi funcion principal
+function principal() {
+  //llamados del dom
 
-const contenedorTotal = document.querySelector("#total");
+  const nav = document.getElementById("navBar");
+  const abrirMenu = document.getElementById("abrir");
+  const cerrarMenu = document.getElementById("cerrar");
+  abrirMenu.addEventListener("click", () => mostrarMenu(nav, abrirMenu));
+  cerrarMenu.addEventListener("click", () => mostrarMenu(nav, abrirMenu));
 
-const btnComprar = document.querySelector(".carrito-acciones-comprar");
-btnComprar.addEventListener("click", comprarCarrito);
+  let carrito = [];
+  localStorage.getItem("carrito")
+    ? (carrito = JSON.parse(localStorage.getItem("carrito")))
+    : (carrito = []);
 
-// funcion de cargar productos, si hay productos en el storage se agregan y añaden clases para cambiar la apariencia, luego se toma el contenedor del carrito en el html y se renderizan los productos llamados del storage creando asi las tarjetas y haciendo un append hacia el contenedor.
-function cargarProductosCarrito() {
-  if (productosEnCarro && productosEnCarro.length > 0) {
-    contenedorCarritoVacio.classList.add("disabled");
-    contenedorCarritoProductos.classList.remove("disabled");
-    carritoAcciones.classList.remove("disabled");
-    carritoComprado.classList.add("disabled");
+  const btnVaciar = document.querySelector(".vaciar__carrito");
+  btnVaciar.addEventListener("click", () => vaciarCarrito(carrito));
 
-    contenedorCarritoProductos.innerHTML = "";
+  const btnComprar = document.querySelector(".comprarCarrito");
+  btnComprar.addEventListener("click", () => comprarCarrito(carrito));
 
-    productosEnCarro.forEach((prod) => {
-      const div = document.createElement("div");
-      div.classList.add("carrito-producto");
-      div.innerHTML = `
-                        <img class="carrito-img" src="${prod.imagen}" alt="${
-        prod.titulo
-      }">
-    
-                        <div class="carrito-producto-titulo">
-                            <small>Titulo</small>
-                            <h3>${prod.titulo}</h3>
-                        </div>
-    
-                        <div class="carrito-producto-cantidad">
-                            <small>Cantidad</small>
-                            <p>${prod.cantidad}</p>
-                        </div>
-    
-                        <div class="carrito-producto-precio">
-                            <small>precio</small>
-                            <p>$${prod.precio}</p>
-                        </div>
-    
-                        <div class="carrito-producto-total">
-                            <small>Subtotal</small>
-                            <p>$${prod.precio * prod.cantidad}</p>
-                        </div>
-                        <button class="carrito-producto-eliminar" id="${
-                          prod.id
-                        }"><i class="bi bi-trash-fill"></i></button>
-                    `;
-
-      contenedorCarritoProductos.append(div);
-    });
-  } else {
-    contenedorCarritoVacio.classList.remove("disabled");
-    contenedorCarritoProductos.classList.add("disabled");
-    carritoAcciones.classList.add("disabled");
-    carritoComprado.classList.add("disabled");
-  }
-
-  actualizarBotonesEliminar();
-  actualizarTotal();
+  renderizarCarrito(carrito);
+  actualizarTotal(carrito);
 }
 
-// se llama a la funcion de cargar productos, para que renderice
-cargarProductosCarrito();
+//function mostrar menu
+function mostrarMenu(nav, menu) {
+  nav.classList.toggle("visible");
+  menu.classList.toggle("not-visible");
+}
 
-// se llaman a los botones de eliminar y se les agrega el onclick y la funcion.
-function actualizarBotonesEliminar() {
-  let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+// funcion renderizar carrito
+function renderizarCarrito(productos) {
+  let contenedor = document.querySelector(".contenedor-carrito");
+  contenedor.innerHTML = "";
 
-  botonesEliminar.forEach((boton) => {
-    boton.addEventListener("click", eliminarDelCarrito);
+  productos.forEach((prod) => {
+    let tarjetaProducto = document.createElement("div");
+    tarjetaProducto.className = "tarjetaCarrito";
+
+    tarjetaProducto.innerHTML = `
+    <img src=${prod.img}>
+    <p>${prod.titulo}</p>
+    <p>$${prod.precioUnitario}</p>
+    <p>Cantidad: ${prod.unidades}</p>
+    <p>$${prod.subtotal}</p>
+    `;
+
+    contenedor.appendChild(tarjetaProducto);
   });
 }
 
-// funcion para eliminar productos, que recibe el evento como parametro y se busca dentro del index un producto cuyo id sea igual al del boton para buscar su posicion, luego de encontrar la posicion si hacemos click se eliminara y se volvera a cargar el carrito para que se actualice.
-function eliminarDelCarrito(e) {
-  const idBoton = e.currentTarget.id;
-  const index = productosEnCarro.findIndex((prod) => prod.id === idBoton);
+// funcion actualizar total
+function actualizarTotal(prod) {
+  let total = document.querySelector(".total");
 
-  Swal.fire({
-    title: "Esta seguro que desea eliminar el producto?",
-    showDenyButton: true,
-    showCancelButton: true,
-    confirmButtonText: "Si",
-    denyButtonText: `No`,
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire({
-        icon: `error`,
-        title: `Producto Eliminado`,
-        showConfirmButton: false,
-        timer: 1000,
-      });
-
-      productosEnCarro.splice(index, 1);
-      cargarProductosCarrito();
-      localStorage.setItem(
-        "productos-en-carrito",
-        JSON.stringify(productosEnCarro)
-      );
-    } else if (result.isDenied) {
-      Swal.fire({
-        icon: `info`,
-        title: `El producto no se elimino`,
-        showConfirmButton: false,
-        timer: 800,
-      });
-    }
-  });
-}
-
-// funcion vaciar carrito, en esta funcion tuve que buscar ejemplos y maneras de vaciarlo ya que no encontraba la manera de hacer andar el clear.
-function vaciarCarrito() {
-  productosEnCarro.length = [];
-  localStorage.setItem(
-    "productos-en-carrito",
-    JSON.stringify(productosEnCarro)
-  );
-  cargarProductosCarrito();
-}
-
-// funcion para actualizar el total, se ejecuta un reduce con el precio de los productos en carro, luego cambia el valor de total convirtiendolo en el precio total del carrito.
-function actualizarTotal() {
-  let templateTotal = productosEnCarro.reduce(
-    (ac, prod) => ac + prod.precio * prod.cantidad,
+  let templateTotal = prod.reduce(
+    (ac, prod) => ac + prod.precioUnitario * prod.unidades,
     0
   );
-  total.innerText = `$${templateTotal}`;
+
+  total.innerText = `Total: $${templateTotal}`;
 }
 
-// en esta funcion lo primero que hacemos es vaciar el carrito y luego damos una alerta de compra.
-function comprarCarrito() {
-  productosEnCarro.length = 0;
-  localStorage.setItem(
-    "productos-en-carrito",
-    JSON.stringify(productosEnCarro)
-  );
-  cargarProductosCarrito();
+// funcion vaciar carrito
+function vaciarCarrito(carrito) {
+  carrito.length = [];
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  renderizarCarrito(carrito);
+  let total = document.querySelector(".total");
+  total.innerText = "Total: $0";
+}
 
-  contenedorCarritoVacio.classList.add("disabled");
-  contenedorCarritoProductos.classList.add("disabled");
-  carritoAcciones.classList.add("disabled");
-  carritoComprado.classList.remove("disabled");
+// comprar carrito
+function comprarCarrito(carrito) {
+  if (carrito.length > 1) {
+    carrito.length = 0;
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    renderizarCarrito(carrito);
 
-  Swal.fire({
-    icon: `success`,
-    title: `Compra recibida`,
-    showConfirmButton: false,
-    timer: 2300,
-  });
+    Swal.fire({
+      icon: `success`,
+      title: `Compra recibida`,
+      showConfirmButton: false,
+      timer: 2300,
+    });
+
+    let total = document.querySelector(".total");
+    total.innerText = "Total: $0";
+  }
 }
--- js/main.js
+++ js/main.js
@@ -1,260 +1,270 @@
-//Array de productos.
-const productos = [
-  //arigos
-  {
-    id: "abrigo-01",
-    titulo: "Abrigo 01",
-    imagen: "./img/abrigos/01.jpg",
-    categoria: {
-      nombre: "Abrigos",
-      id: "abrigos",
+principal();
+//mi funcion principal
+function principal() {
+  //array de productos
+  const productos = [
+    //abrigos
+    {
+      id: "abrigo-01",
+      titulo: "Abrigo 01",
+      imagen: "./img/abrigos/01.jpg",
+      categoria: {
+        nombre: "Abrigos",
+        id: "abrigos",
+      },
+      precio: 1000,
     },
-    precio: 1000,
-  },
-  {
-    id: "abrigo-02",
-    titulo: "Abrigo 02",
-    imagen: "./img/abrigos/02.jpg",
-    categoria: {
-      nombre: "Abrigos",
-      id: "abrigos",
+    {
+      id: "abrigo-02",
+      titulo: "Abrigo 02",
+      imagen: "./img/abrigos/02.jpg",
+      categoria: {
+        nombre: "Abrigos",
+        id: "abrigos",
+      },
+      precio: 1000,
     },
-    precio: 1000,
-  },
-  {
-    id: "abrigo-03",
-    titulo: "Abrigo 03",
-    imagen: "./img/abrigos/03.jpg",
-    categoria: {
-      nombre: "Abrigos",
-      id: "abrigos",
+    {
+      id: "abrigo-03",
+      titulo: "Abrigo 03",
+      imagen: "./img/abrigos/03.jpg",
+      categoria: {
+        nombre: "Abrigos",
+        id: "abrigos",
+      },
+      precio: 1000,
     },
-    precio: 1000,
-  },
-  {
-    id: "abrigo-04",
-    titulo: "Abrigo 04",
-    imagen: "./img/abrigos/04.jpg",
-    categoria: {
-      nombre: "Abrigos",
-      id: "abrigos",
+    {
+      id: "abrigo-04",
+      titulo: "Abrigo 04",
+      imagen: "./img/abrigos/04.jpg",
+      categoria: {
+        nombre: "Abrigos",
+        id: "abrigos",
+      },
+      precio: 1000,
     },
-    precio: 1000,
-  },
-  {
-    id: "abrigo-05",
-    titulo: "Abrigo 05",
-    imagen: "./img/abrigos/05.jpg",
-    categoria: {
-      nombre: "Abrigos",
-      id: "abrigos",
+    {
+      id: "abrigo-05",
+      titulo: "Abrigo 05",
+      imagen: "./img/abrigos/05.jpg",
+      categoria: {
+        nombre: "Abrigos",
+        id: "abrigos",
+      },
+      precio: 1000,
     },
-    precio: 1000,
-  },
-  //pantalones
-  {
-    id: "pantalo-01",
-    titulo: "pantalon 01",
-    imagen: "./img/pantalones/01.jpg",
-    categoria: {
-      nombre: "Pantalones",
-      id: "pantalones",
+    //pantalones
+    {
+      id: "pantalo-01",
+      titulo: "pantalon 01",
+      imagen: "./img/pantalones/01.jpg",
+      categoria: {
+        nombre: "Pantalones",
+        id: "pantalones",
+      },
+      precio: 5000,
     },
-    precio: 5000,
-  },
-  {
-    id: "pantalo-02",
-    titulo: "pantalon 02",
-    imagen: "./img/pantalones/02.jpg",
-    categoria: {
-      nombre: "Pantalones",
-      id: "pantalones",
+    {
+      id: "pantalo-02",
+      titulo: "pantalon 02",
+      imagen: "./img/pantalones/02.jpg",
+      categoria: {
+        nombre: "Pantalones",
+        id: "pantalones",
+      },
+      precio: 5000,
     },
-    precio: 5000,
-  },
-  {
-    id: "pantalo-03",
-    titulo: "pantalon 03",
-    imagen: "./img/pantalones/03.jpg",
-    categoria: {
-      nombre: "Pantalones",
-      id: "pantalones",
+    {
+      id: "pantalo-03",
+      titulo: "pantalon 03",
+      imagen: "./img/pantalones/03.jpg",
+      categoria: {
+        nombre: "Pantalones",
+        id: "pantalones",
+      },
+      precio: 5000,
     },
-    precio: 5000,
-  },
-  {
-    id: "pantalo-04",
-    titulo: "pantalon 04",
-    imagen: "./img/pantalones/04.jpg",
-    categoria: {
-      nombre: "Pantalones",
-      id: "pantalones",
+    {
+      id: "pantalo-04",
+      titulo: "pantalon 04",
+      imagen: "./img/pantalones/04.jpg",
+      categoria: {
+        nombre: "Pantalones",
+        id: "pantalones",
+      },
+      precio: 5000,
     },
-    precio: 5000,
-  },
-  {
-    id: "pantalo-05",
-    titulo: "pantalon 05",
-    imagen: "./img/pantalones/05.jpg",
-    categoria: {
-      nombre: "Pantalones",
-      id: "pantalones",
+    {
+      id: "pantalo-05",
+      titulo: "pantalon 05",
+      imagen: "./img/pantalones/05.jpg",
+      categoria: {
+        nombre: "Pantalones",
+        id: "pantalones",
+      },
+      precio: 5000,
     },
-    precio: 5000,
-  },
-  //camisetas
-  {
-    id: "camiseta-01",
-    titulo: "camiseta 01",
-    imagen: "./img/camisetas/01.jpg",
-    categoria: {
-      nombre: "Camisetas",
-      id: "camisetas",
+    //camisetas
+    {
+      id: "camiseta-01",
+      titulo: "camiseta 01",
+      imagen: "./img/camisetas/01.jpg",
+      categoria: {
+        nombre: "Camisetas",
+        id: "camisetas",
+      },
+      precio: 3000,
     },
-    precio: 3000,
-  },
-  {
-    id: "camiseta-02",
-    titulo: "camiseta 02",
-    imagen: "./img/camisetas/02.jpg",
-    categoria: {
-      nombre: "Camisetas",
-      id: "camisetas",
+    {
+      id: "camiseta-02",
+      titulo: "camiseta 02",
+      imagen: "./img/camisetas/02.jpg",
+      categoria: {
+        nombre: "Camisetas",
+        id: "camisetas",
+      },
+      precio: 3000,
     },
-    precio: 3000,
-  },
-  {
-    id: "camiseta-03",
-    titulo: "camiseta 03",
-    imagen: "./img/camisetas/03.jpg",
-    categoria: {
-      nombre: "Camisetas",
-      id: "camisetas",
+    {
+      id: "camiseta-03",
+      titulo: "camiseta 03",
+      imagen: "./img/camisetas/03.jpg",
+      categoria: {
+        nombre: "Camisetas",
+        id: "camisetas",
+      },
+      precio: 3000,
     },
-    precio: 3000,
-  },
-  {
-    id: "camiseta-04",
-    titulo: "camiseta 04",
-    imagen: "./img/camisetas/04.jpg",
-    categoria: {
-      nombre: "Camisetas",
-      id: "camisetas",
+    {
+      id: "camiseta-04",
+      titulo: "camiseta 04",
+      imagen: "./img/camisetas/04.jpg",
+      categoria: {
+        nombre: "Camisetas",
+        id: "camisetas",
+      },
+      precio: 3000,
     },
-    precio: 3000,
-  },
-  {
-    id: "camiseta-05",
-    titulo: "camiseta 05",
-    imagen: "./img/camisetas/05.jpg",
-    categoria: {
-      nombre: "Camisetas",
-      id: "camisetas",
+    {
+      id: "camiseta-05",
+      titulo: "camiseta 05",
+      imagen: "./img/camisetas/05.jpg",
+      categoria: {
+        nombre: "Camisetas",
+        id: "camisetas",
+      },
+      precio: 3000,
     },
-    precio: 3000,
-  },
-];
-// Llamados del DOM.
-const contenedorProductos = document.querySelector(".contenedor-productos");
-const botonesCategorias = document.querySelectorAll(".boton-categoria");
-const tituloPrincipal = document.querySelector("#tituloPrincipal");
-const botonesAgregar = document.querySelectorAll(".producto-agregar");
-const numeroDelCarrito = document.querySelector("#numero-carrito");
+  ];
+  //llamados del dom
+  const botonesCategorias = document.querySelectorAll(".boton-categoria");
+  const tituloPrincipal = document.querySelector(".titulo__productos");
 
-// funcion de cargar productos
-function cargarProductos(productosElegidos) {
-  contenedorProductos.innerHTML = "";
+  const nav = document.getElementById("navBar");
+  const abrirMenu = document.getElementById("abrir");
+  const cerrarMenu = document.getElementById("cerrar");
+  abrirMenu.addEventListener("click", () => mostrarMenu(nav, abrirMenu));
+  cerrarMenu.addEventListener("click", () => mostrarMenu(nav, abrirMenu));
 
-  productosElegidos.forEach((producto) => {
-    const div = document.createElement("div");
-    div.classList.add("producto");
-    div.innerHTML = `
-            <img class="producto-img" src="${producto.imagen}" alt="${producto.titulo}">
-            <div class="producto-info">
-                <h3 class="producto-titulo">${producto.titulo}</h3>
-                <p class="producto-precio">$${producto.precio}</p>
-            <button class="producto-agregar" id="${producto.id}">Agregar</button>
-            </div>
-        `;
-
-    contenedorProductos.append(div);
-  });
-  actualizarBotonesAgregar();
-}
-cargarProductos(productos);
-
-// filtrar con los botones de categoria
-botonesCategorias.forEach((boton) => {
-  boton.addEventListener("click", (evento) => {
-    botonesCategorias.forEach((boton) => {
-      boton.classList.remove("active");
-    });
-
-    evento.currentTarget.classList.add("active");
-
-    if (evento.currentTarget.id != "todos") {
-      const productoCategoria = productos.find(
-        (prod) => prod.categoria.id === evento.currentTarget.id
-      );
-      tituloPrincipal.innerText = productoCategoria.categoria.nombre;
-
-      const productosBoton = productos.filter(
-        (producto) => producto.categoria.id === evento.currentTarget.id
-      );
-      cargarProductos(productosBoton);
-    } else {
-      tituloPrincipal.innerText = "Todos los productos";
-      cargarProductos(productos);
-    }
-  });
-});
-
-// botones de agregar al carrito y llamado de la funcion agregar.
-function actualizarBotonesAgregar() {
-  let botonesAgregar = document.querySelectorAll(".producto-agregar");
-
-  botonesAgregar.forEach((boton) => {
-    boton.addEventListener("click", agregarAlCarrito);
-  });
-}
-
-// productos en carrito desde el local storage donde si existen los convierte y si no queda vacio.
-let productosEnCarrito;
-let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
-
-if (productosEnCarritoLS) {
-  productosEnCarrito = JSON.parse(productosEnCarritoLS);
-  actualizarNumeroCarrito();
-} else {
-  productosEnCarrito = [];
-}
-
-// funcion de agregar al carrito donde toma el evento como parametro el cual tiene el mismo id del producto que le corresponda. luego se busca si algun objeto tiene el mismo id y si lo tiene se le suma una unidad a cantidad y si no coincide ningun objeto, este se carga al array productos en carrito. Al final es subido al local storage con la convercion JSON.stringify,
-function agregarAlCarrito(e) {
-  const idBoton = e.currentTarget.id;
-  const productoAgregado = productos.find((prod) => prod.id === idBoton);
-
-  if (productosEnCarrito.some((prod) => prod.id === idBoton)) {
-    const index = productosEnCarrito.findIndex((prod) => prod.id === idBoton);
-    productosEnCarrito[index].cantidad++;
+  let carrito = [];
+  if (localStorage.getItem("carrito")) {
+    carrito = JSON.parse(localStorage.getItem("carrito"));
   } else {
-    productoAgregado.cantidad = 1;
-    productosEnCarrito.push(productoAgregado);
+    carrito = [];
+  }
+  renderizarTarjetas(productos, carrito);
+  filtrarProductos(botonesCategorias, tituloPrincipal, productos, carrito);
+}
+
+//function mostrar menu
+function mostrarMenu(nav, menu) {
+  nav.classList.toggle("visible");
+  menu.classList.toggle("not-visible");
+}
+
+//filtrar productos
+function filtrarProductos(
+  botonesCategorias,
+  tituloPrincipal,
+  productos,
+  carrito
+) {
+  botonesCategorias.forEach((boton) => {
+    boton.addEventListener("click", (evento) => {
+      if (evento.currentTarget.id != "todos") {
+        const productoCategoria = productos.find(
+          (prod) => prod.categoria.id === evento.currentTarget.id
+        );
+        tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+
+        const productosBoton = productos.filter(
+          (producto) => producto.categoria.id === evento.currentTarget.id
+        );
+        renderizarTarjetas(productosBoton, carrito);
+      } else {
+        tituloPrincipal.innerText = "Todos los productos";
+        renderizarTarjetas(productos, carrito);
+      }
+    });
+  });
+}
+
+// function para renderizar prods
+function renderizarTarjetas(prod, carrito) {
+  const contenedor = document.querySelector("#contenedor__productos");
+  contenedor.innerHTML = "";
+
+  prod.forEach((producto) => {
+    const tarjeta = document.createElement("div");
+
+    tarjeta.innerHTML = `<div class="producto">
+    <img src="${producto.imagen}">
+    <div class="producto__info">
+    <p class="producto__info-title">${producto.titulo}</p>
+    <p class="precio">$${producto.precio}</p>
+    <button class="button__agregar" id=${producto.id}>Agregar al Carrito</button>
+    </div>
+    </div>`;
+
+    contenedor.appendChild(tarjeta);
+
+    let botonCarrito = document.getElementById(producto.id);
+    botonCarrito.addEventListener("click", (e) =>
+      agregarAlCarrito(prod, e, carrito)
+    );
+  });
+}
+
+// funcion agregar al carrito
+function agregarAlCarrito(producto, evento, carrito) {
+  let productoOriginal = producto.find((prod) => prod.id === evento.target.id);
+
+  let productoEnCarrito = carrito.find(
+    (prod) => prod.id === productoOriginal.id
+  );
+
+  if (productoEnCarrito) {
+    productoEnCarrito.unidades++;
+    productoEnCarrito.subtotal = Number(
+      productoEnCarrito.precioUnitario * productoEnCarrito.unidades
+    );
+  } else {
+    carrito.push({
+      img: productoOriginal.imagen,
+      id: productoOriginal.id,
+      titulo: productoOriginal.titulo,
+      precioUnitario: Number(productoOriginal.precio),
+      unidades: 1,
+      subtotal: Number(productoOriginal.precio),
+    });
   }
 
-  actualizarNumeroCarrito();
+  //localStorage
 
-  localStorage.setItem(
-    "productos-en-carrito",
-    JSON.stringify(productosEnCarrito)
-  );
+  let carritoJSON = JSON.stringify(carrito);
+  localStorage.setItem("carrito", carritoJSON);
 }
 
-// funcion que actualiza el numero de productos del carrito tomando la cantidad de unidades de cada producto en el local storage.
-function actualizarNumeroCarrito() {
-  let numeroCarrito = productosEnCarrito.reduce(
-    (ac, prod) => ac + prod.cantidad,
-    0
-  );
-  numeroDelCarrito.innerText = numeroCarrito;
-}
+// funcion renderizar carrito
+function renderizarCarrito(productos) {}
